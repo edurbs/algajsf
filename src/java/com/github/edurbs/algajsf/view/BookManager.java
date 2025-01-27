908af6b67f2af60711de79195b2d548e8f9bfab5
Eduardo  Soares <edurbs@gmail.com>
2025-01-27 16:29:56 -0300

    add: links

--- src/java/com/github/edurbs/algajsf/view/BookManager.java
+++ src/java/com/github/edurbs/algajsf/view/BookManager.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ValueChangeEvent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +18,9 @@ public class BookManager implements Serializable {
     
     private List<Book> books;
     private Book book;
+    private Book selectedBook;
+    private String titleSearch;
+    private List<Book> filteredBooks=new ArrayList<>();
 
     public BookManager() {
         books = new ArrayList<>();
@@ -27,5 +31,38 @@ public class BookManager implements Serializable {
         books.add(book);
         book = new Book();
     }
-   
+    
+    public void remove(){
+        books.remove(selectedBook);
+    }
+    
+    public String open(){
+        return "Bookshelf?faces-redirect=true";
+    }
+    
+    public String startPage(){
+        return "start?faces-redirect=true";
+    }
+    
+    public String help(){
+        if(books.isEmpty()){
+            return "basicHelp?faces-redirect=true";
+        }else{
+            return "fullHelp?faces-redirect=true";
+        }
+    }
+    
+    public void onTitleChanged(ValueChangeEvent event){
+        filteredBooks.clear();
+        for (Book book : books) {
+            if(book.getTitle()!= null
+                    && book.getTitle().toLowerCase()
+                            .startsWith(event.getNewValue().toString().toLowerCase())){
+                filteredBooks.add(book);
+            }
+        }
+               
+        
+    }
+    
 }
